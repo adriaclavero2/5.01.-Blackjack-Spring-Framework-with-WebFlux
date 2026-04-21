521d7cae854cbde9d3abf753a6f6d4f25d3d2bc2
adriaclavero2 <adriaclavero2@gmail.com>
2026-04-21 10:15:21 +0200

    Fix database connection configuration for Docker setup

---
+++ src/main/java/com/blackjack/blackjack_api/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.blackjack.blackjack_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebFluxConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
